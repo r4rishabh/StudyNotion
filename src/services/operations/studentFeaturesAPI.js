cb3c96dde9075806cd7e42c064e501e5e39f23e3
Rishabh Dev Singh <rishabhdevsingh@Bubbbleb.local>
2025-04-08 12:00:09 +0530

    Testing PAyment

--- src/services/operations/studentFeaturesAPI.js
+++ src/services/operations/studentFeaturesAPI.js
@@ -66,7 +66,7 @@ export async function BuyCourse(
 
     // Opening the Razorpay SDK
     const options = {
-      key: process.env.VITE_REACT_RAZORPAY_KEY,
+      key: process.env.REACT_APP_RAZORPAY_KEY,
       currency: orderResponse.data.data.currency,
       amount: `${orderResponse.data.data.amount}`,
       order_id: orderResponse.data.data.id,
@@ -94,6 +94,7 @@ export async function BuyCourse(
     toast.error("Could Not make Payment.")
   }
   toast.dismiss(toastId)
+  console.log("Environment Variable :", process.env.VITE_REACT_RAZORPAY_KEY )
 }
 
 // Verify the Payment
